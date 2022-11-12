5b2fb63ac86b59fb99fc0a78d597a6574eca7570
Póllai Róbert <robert.pollai@r-szoft.hu>
2022-11-12 07:59:52 +0100

    TEST-2 - add commit

--- src/main/java/com/rs/numbergame/NumbergameApplication.java
+++ src/main/java/com/rs/numbergame/NumbergameApplication.java
@@ -3,11 +3,14 @@ package com.rs.numbergame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point to spring app
+ */
 @SpringBootApplication
 public class NumbergameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NumbergameApplication.class, args);
-		
+		System.out.println("Welcome Gamer");
 	}
 }
